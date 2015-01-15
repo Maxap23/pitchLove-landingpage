11879f48e8b8bd4801763a2ccc8469ee0308fa64
Maxime Paul <maxime.a.paul@gmail.com>
2015-01-15 13:51:33 -0500

    Fixed form mailer

--- assets/inc/contact_me.php
+++ assets/inc/contact_me.php
@@ -1,8 +1,14 @@
 <?php 
 // check if fields passed are empty 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require ("PHPMailerAutoload.php");
  
- if(empty($_POST['name'])   ||    
+if(empty($_POST['name'])   ||    
     empty($_POST['email'])  ||
+    empty($_POST['ideaname'])  ||
+    empty($_POST['problem'])  ||
+    empty($_POST['needs'])  ||
     empty($_POST['message'])||   
     !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))    
   {     
@@ -11,16 +17,52 @@
      
   $name = $_POST['name']; 
   $email_address = $_POST['email']; 
+    $ideaname = $_POST['ideaname'];
+    $problem = $_POST['problem'];
+    $needs = $_POST['needs'];
   $message = $_POST['message'];      
- 
+
  // create email body and send it    
- $to = 'info@e2.is'; 
+$to = 'maxime@e2.is';
+	$mail = new PHPMailer();
+	$host = 'smtp.gmail.com'; 
+	$port = 465;
+	$encrypted = 'ssl';
+	$username = 'maxime@e2.is';
+	$password = 'oopqylfajgjcrhui';
+	//DO this if you need to set from address, I assume that we dont need it
+	
+	$from_name = 'pitchLove Helps';
+	$from = 'info@e2.is';
+	$mail->From = $from;
+	$mail->FromName = $from_name;
+	
+	//config send mail
+	$mail->IsSMTP();
+	//$mail->SMTPDebug = 2;
+	//$mail->Debugoutput = "html";
+	$mail->Host = $host;
+	$mail->Port = $port;
+	$mail->SMTPSecure = $encrypted;
+    $mail->SMTPAuth = true;	
+	$mail->Username = $username; 
+	$mail->Password = $password;
+	$mail->CharSet = 'UTF-8';
+	$mail->IsHTML(true);
+
  // put your email 
- $email_subject = "Contact form submitted by:  $name"; $email_body = "You have received a new message. \n\n".                 
-                   " Here are the details:\n \nName: $name \n ".                  
-                   "Email: $email_address\n Message \n $message"; 
- $headers = "From: contacts@pitchlove.co\n"; 
+ $email_subject = "Contact form submitted by:  $name"; 
+$email_body = "You have received a new message. \n\n".                 
+                   " Here are the details:  <b>Name:</b> $name  ".                  
+                   "  <b>Email:</b> $email_address<br><br><b>Idea:</b> $ideaname <br> <b>Problem:</b> $problem <br> <b>Needs:</b> $needs <br> <b>Pitch:</b> $message"; 
+ $headers = "From: gethelp@pitchlove.co\n"; 
  $headers .= "Reply-To: $email_address";     
- 
- mail($to,$email_subject,$email_body,$headers); return true;             
+
+  $mail->Subject = $email_subject;
+	$mail->Body = $email_body;
+    $mail->AddAddress($to);
+
+	$statusSend = $mail->Send();
+
+return $statusSend;             
 ?>
--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -1,7 +1,4 @@
-/*
-  Jquery Validation using jqBootstrapValidation
-   example is taken from jqBootstrapValidation docs
-  */
+
 $(function() {
  
  $("input,textarea").jqBootstrapValidation(
@@ -16,43 +13,41 @@ $(function() {
        // get values from FORM
        var name = $("input#name").val();
        var email = $("input#email").val();
+         var ideaname = $("input#ideaname").val();
+         var problem = $("input#problem").val();
+         var needs = $("input#needs").val();
        var message = $("textarea#message").val();
         var firstName = name; // For Success/Failure Message
            // Check for white space in name for Success/Fail message
+         
         if (firstName.indexOf(' ') >= 0) {
        firstName = name.split(' ').slice(0, -1).join(' ');
          }
      $.ajax({
-                url: "./inc/contact_me.php",
+                url: "http://pitchlove.co/assets/inc/contact_me.php",
                 type: "POST",
-                data: {name: name, email: email, message: message},
+                data: {name: name, email: email, ideaname: ideaname, problem: problem, needs: needs, message: message},
                 cache: false,
                 success: function() {
                 // Success message
-                   $('#success').html("</pre>
-<div class="alert alert-success">");
- $('#success > .alert-success').html("<button class="close" type="button" data-dismiss="alert">×")
+                   $('#success').html('<div class="alert alert-success">');
+ $('#success > .alert-success').html('<button class="close" type="button" data-dismiss="alert">×')
  .append( "</button>");
  $('#success > .alert-success')
- .append("<strong>Your message has been sent. </strong>");
+ .append("<strong>Your message has been sent. You will soon receive an email with the next steps.</strong>");
  $('#success > .alert-success')
- .append('</div>
-<pre>
-');
+ .append('</div>');
  
           //clear all fields
           $('#contactForm').trigger("reset");
           },
        error: function() {
         // Fail message
-         $('#success').html("</pre>
-<div class="alert alert-danger">");
- $('#success > .alert-danger').html("<button class="close" type="button" data-dismiss="alert">×")
+         $('#success').html('<div class="alert alert-danger">');
+ $('#success > .alert-danger').html('<button class="close" type="button" data-dismiss="alert">×')
  .append( "</button>");
- $('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href="'mailto:info@e2.is?Subject=Message_Me">info@e2.is</a> ? Sorry for the inconvenience!");
- $('#success > .alert-danger').append('</div>
-<pre>
-');
+ $('#success > .alert-danger').append("<strong>Sorry "+firstName+' it seems that my mail server is not responding...</strong> Could you please email me directly to <a href="mailto:info@e2.is?Subject=Form Issue">info@e2.is</a> ? Sorry for the inconvenience!');
+ $('#success > .alert-danger').append('</div>');
         //clear all fields
         $('#contactForm').trigger("reset");
         },
